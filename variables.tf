746408430918191a3886bacda4be8ed4e2fd196a
Henrique Fukuya <henriquefukuya@gmail.com>
2025-03-14 16:45:45 -0300

    Configurar provider azurerm com variaveis de auth

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ARM_CLIENT_ID" {
+  description = "Client ID da aplicação Azure AD"
+  type        = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "Client Secret da aplicação Azure AD"
+  type        = string
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "ID da assinatura Azure"
+  type        = string
+}
+
+variable "ARM_TENANT_ID" {
+  description = "Tenant ID da sua conta Azure"
+  type        = string
+}
